18beb4730a4c7a053b08f87501b3d08b0e024c5c
Fabrizio Lagioia <meandi958@gmail.com>
2024-03-26 16:04:10 +0100

    fatte correzioni classe Product

--- src/Product.java
+++ src/Product.java
@@ -13,22 +13,14 @@ public class Product {
     }
 
     public Product(String name, double unitPrice, int quantity) throws InvalidProductParameterException {
-        if (name.isEmpty()) {
-            throw new InvalidProductParameterException("name non può essere vuoto");
-        } else {
-            name = name;
-        }
-        if (quantity <= 0) {
-            throw new InvalidProductParameterException("quantity deve essere maggiore di zero");
-        } else {
-            this.quantity = quantity;
-        }
-        if (unitPrice <= 0.0) {
-            throw new InvalidProductParameterException("unitPrice deve essere maggiore di zero");
-        } else {
-            this.unitPrice = unitPrice;
-        }
+        verifyNameIntegrity(name);
+        verifyUnitPriceIntegrity(unitPrice);
+        verifyQuantityIntegrity(quantity);
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
     }
+
 
     public String getName() {
         return name;
@@ -43,29 +35,39 @@ public class Product {
     }
 
     public void setName(String name) {
-        if (name.isEmpty()) {
-            throw new InvalidProductParameterException("name non può essere vuoto");
-        } else {
-            this.name = name;
-        }
+        verifyNameIntegrity(name);
+        this.name = name;
     }
 
     public void setUnitPrice(double unitPrice) {
-        if (unitPrice <= 0.0) {
-            throw new InvalidProductParameterException("unitPrice deve essere maggiore di zero");
-        } else {
-            this.unitPrice = unitPrice;
-        }
+        verifyUnitPriceIntegrity(unitPrice);
+        this.unitPrice = unitPrice;
     }
 
     public void setQuantity(int quantity) {
-        if (quantity <= 0) {
-            throw new InvalidProductParameterException("quantity deve essere maggiore di zero");
-        } else {
-            this.quantity = quantity;
+        verifyQuantityIntegrity(quantity);
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return unitPrice * quantity;
+    }
+
+    private void verifyNameIntegrity(String name) {
+        if (name.isEmpty()) {
+            throw new InvalidProductParameterException("name non può essere vuoto");
         }
     }
-    public double getTotalPrice() {
-        return(getUnitPrice() * getQuantity());
+
+    private void verifyUnitPriceIntegrity(double unitPrice) {
+        if (unitPrice <= 0) {
+            throw new InvalidProductParameterException("unitPrice deve essere maggiore di zero");
+        }
+    }
+
+    private void verifyQuantityIntegrity(int quantity) {
+        if (quantity <= 0) {
+            throw new InvalidProductParameterException("quantity deve essere maggiore di zero");
+        }
     }
 }
